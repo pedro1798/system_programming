24fa3282ebd0b3b3c683e80500ec513408f625cd
Ubuntu <ubuntu@ip-172-31-42-172.ap-northeast-2.compute.internal>
2025-09-09 11:41:10 +0000

    Minor change

--- src/0809/cp1.c
+++ src/0809/cp1.c
@@ -19,20 +19,23 @@ int main(int ac, char *av[]) {
 
     if (ac != 3) { /* check args */
         fprintf(stderr, "usage:%s source destination\n", *av);
+        /* av어레이의 첫 번째 요소는 파일 이름 */
         exit(1);
     }
 
     /* open files */
     if ((in_fd = open(av[1], O_RDONLY)) == -1) { 
-        oops("Cannot open", av[1]);
+        /* 매개변수로 받은 src-file을 연다.  */
+        oops("Cannot open", av[1]); /* 에러처리 */
     }
 
     if ((out_fd = creat(av[2], COPYMODE)) == -1) { 
-        oops("Cannot create", av[2]);
+        /* 매개변수로 받은 dest-file을 생성한다 */
+        oops("Cannot create", av[2]); /* 에러처리 */
     }
 
     /* copy files */
-    while((n_chars = read(in_fd, buf, BUFFERSIZE)) > 0) {
+    while((n_chars = read(in_fd, buf, BUFFERSIZE)) > 0) { /* 읽어온 데이터가 있을 때 */
         if (write(out_fd, buf, n_chars) != n_chars) {
             oops("Write error to", av[2]);
         }
@@ -46,7 +49,17 @@ int main(int ac, char *av[]) {
 }
 
 void oops(char *s1, char *s2) {
-    fprintf(stderr, "error:%s", s1);
+    fprintf(stderr, "error:%s", s1); 
+    /* stderr: 표준 에러 출력 스트림(fd 2)
+     * 버퍼링되지 않고 바로 출력된다.
+     * 프로그래머가 지정한 문자열을 출력한다.
+     */
+    
     perror(s2);
+    /* perror는 직전에 실패한 시스템 콜이나 라이브러리 함수가 남겨놓은 
+     * 전역 변수 errno 값을 확인해서, 그에 맞는 시스템 에러 메시지를 출력합니다.
+     * 출력 형식: 
+     *  s2: <시스템에 정의된 에러 설명 문자열>
+     */
     exit(1);
 }
