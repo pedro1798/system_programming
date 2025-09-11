67b868bde326e9d769d11d4db669b3ff92eccd59
pedro1798 <peter584aa@knu.ac.kr>
2025-09-11 08:26:04 +0000

    update who2 who3

--- 0809/who3/src/who3.c
+++ 0809/who3/src/who3.c
@@ -2,8 +2,10 @@
  *        - surpresses empty records
  *        - formats time nicely
  *        - buffers input (using utmplib)
+ * utmp_open, utmp_next, utmp_reload, utmp_close
  */
 
+#include <time.h> /* formats time nicely */
 #include <stdio.h>
 #include <stdlib.h>
 #include <utmp.h>
@@ -15,22 +17,23 @@
 
 #define SHOWHOST
 
-void showtime(time_t);
+void showtime(long);
 void show_info(struct utmp *utbufp); 
 
 int main() {
-    struct utmp *utbufp, 
-                *utmp_next();
+    struct utmp *utbufp; 
+    /* struct utmp *utmp_next(); utmplib.c에서 정의 */
+
     if (utmp_open(UTMP_FILE) == -1) {
-        perror(UTMP_FILE);
+        perror(UTMP_FILE); /* 오픈 시 에러 처리 */
         exit(1);
     }
     while ((utbufp = utmp_next()) != (struct utmp *) NULL) {
+        /* utbufp가 NULL을 가리키지 않을 때 while 반복 */
         show_info(utbufp);
     }
-    utmp_close();
+    utmp_close(); /* utmplib.c 함수 */
     return 0;
-
 }
 
 /* show_info()
@@ -46,11 +49,17 @@ void show_info(struct utmp *utbufp) {
     printf(" ");
     printf("%-8.8s", utbufp->ut_line);
     printf(" ");
-    printf("%-8.8d", utbufp->ut_time);
+    showtime(utbufp->ut_time);
     printf(" ");
 
 #ifdef SHOWHOST /* if SHOWHOST is defined, compile this code */
     printf("(%s)", utbufp->ut_host);
 #endif
     printf("\n");
+}
+
+void showtime(long timeval) {
+    char *cp;
+    cp = ctime(&timeval); /* epoch time -> 문자열 변환 */
+    printf("%12.12s", cp+4);
 }
