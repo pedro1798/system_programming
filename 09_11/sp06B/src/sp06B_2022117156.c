94c37c07087f17b261a8abbcb564db06c06a6f6f
pedro1798 <peter584aa@knu.ac.kr>
2025-11-06 11:23:17 +0000

    fully updated

--- 09_11/sp06B/src/sp06B_2022117156.c
+++ 09_11/sp06B/src/sp06B_2022117156.c
@@ -7,12 +7,6 @@
 #include <signal.h>
 #define MAXARGS 20 /* cmdline args */
 #define ARGLEN 100 /* token length */
-/* 
- * 1. 입력을 받는다 
- * 2. 받은 입력으로 while문 돌린다
- * 3. while문에서 실행하고 마지막에서 다시 입력을 받는다
- * 4. exit이면 종료한다
- */
 int get_arg_list(char **arglist); 
 void execute(char** arglist);
 
@@ -20,26 +14,28 @@ int main() {
     /*
      * use fork and execvp and wait to do it
      */
-    char **arglist_ptr = (char**)malloc((MAXARGS + 1) * sizeof(char*));
+    char **arglist_ptr = (char**)malloc((MAXARGS + 1) * sizeof(char*)); // 명령어, 플래그 리스트
     
     arglist_ptr[0] = NULL; /* 루프 시작 전에 유효하도록 NULL 초기화 */
     /* 무한 루프 */
-    int arg_count = 0;
+    int arg_count = 0; // 인자 개수 
+
     while(1) {
         for (int i = 0; i < arg_count + 1; i++) {
-            free(arglist_ptr[i]);
+            free(arglist_ptr[i]); // 우선 malloc으로 할당된 인자를 free시킨다.
         }
         
-        arg_count = get_arg_list(arglist_ptr);
+        arg_count = get_arg_list(arglist_ptr); // 인자, 인자 개수 업데이트
         
         if (arglist_ptr[0] == NULL) continue; // 빈 입력 건너뛰기
         if (strcmp(arglist_ptr[0], "exit") == 0) break; // exit 명령 시 종료 
-        // 실행
         
+        // 실행
         if (arg_count > 0) {
             execute(arglist_ptr);
         }
     }
+
     free(arglist_ptr); // 메모리 해제
     return 0;
 }
@@ -62,7 +58,7 @@ void execute(char** arglist_ptr) {
             }
             perror("execvp failed"); /* execvp 정상적으로 실행되면 실행 안 됨 */
             exit(1);
-        default:
+        default: // 실행했던 자식 프로세스가 종료될 때 까지 wait한다. 
             while(wait(&exitstatus) != pid) {
                 printf("child exited with status %d,%d\n", 
                         exitstatus>>8, exitstatus&0377);
@@ -87,8 +83,8 @@ int get_arg_list(char** arglist_ptr) { /* argbuf에 입력 받고 arglist_ptr에
      *  s를 반환한다. 아무 문자도 읽지 못했을 때만 NULL을 반환한다.
      */
     if (fgets(argbuf, ARGLEN, stdin)) { // argbuf 버퍼에 인수 문자열 받는다
-        if (argbuf[strlen(argbuf)-1] == '\n') {
-            argbuf[strlen(argbuf)-1] = '\0';
+        if (argbuf[strlen(argbuf)-1] == '\n') { // fgets는 \n까지 입력받는다. 
+            argbuf[strlen(argbuf)-1] = '\0'; // EOF
         }
         /* 
          * 현재 실행 중인 프로세스의 메모리 이미지를 새로운 프로그램의 메모리
@@ -105,17 +101,17 @@ int get_arg_list(char** arglist_ptr) { /* argbuf에 입력 받고 arglist_ptr에
 
         while (token != NULL && arg_count < MAXARGS) {
             /* 토큰(문자열)의 주소를 새로운 배열에 저장 */
-            char *cp_token = (char*)malloc((MAXARGS+1) * strlen(token));
-            cp_token = strdup(token);
-            if (cp_token == NULL) {
+            char *cp_token = (char*)malloc((MAXARGS+1) * strlen(token)); // heap 영역에 토큰 복사 후 저장
+            cp_token = strdup(token); // 토큰 복사
+            if (cp_token == NULL) { // 에러처리
                 perror("strdup");
                 exit(1);
             }
-            arglist_ptr[arg_count++] = cp_token;
+            arglist_ptr[arg_count++] = cp_token; // 토큰을 인자 배열에 저장
             
             token = strtok(NULL, " "); /* 다음 토큰 가져오기 */
         }
         arglist_ptr[arg_count] = NULL; /* 배열의 끝 표시 */
     }
-    return arg_count;
+    return arg_count; // 토큰 개수 리턴
 }
