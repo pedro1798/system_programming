a3623768625f3cc2ddb9e722baa8ddb88d6c765f
pedro1798 <peter584aa@knu.ac.kr>
2025-11-11 02:35:26 +0000

    update tetris

--- tetris/src/tetris.c
+++ tetris/src/tetris.c
@@ -21,8 +21,13 @@ int main() {
     int term_height, term_width;
     getmaxyx(stdscr, term_height, term_width); // 터미널 크기 얻기
     
-    int box_height = term_height / 3 * 2;
-    int box_width = term_width / 3 * 2;
+    int box_height = term_height * 2 / 3;
+    int box_width = term_width * 2 / 3;
+    
+    if (box_height > term_height - 2) box_height = term_height - 2;
+    if (box_width  > term_width  - 2) box_width  = term_width  - 2;
+
+
     int start_y = (term_height - box_height) / 2;
     int start_x = (term_width - box_width) / 2;
 
@@ -32,10 +37,13 @@ int main() {
     char msg[] = "Hello, ncurses world!";
     int msg_y = box_height / 2;
     int msg_x = (box_width - (int)strlen(msg)) / 2;
+    
+    if (msg_y == box_height - 1) msg_y--;  // 경계 침범 방지
+
     mvwprintw(win, msg_y, msg_x, "%s", msg);
 
     wrefresh(win); // 윈도우 갱신(화면에 실제로 그림)
-    refresh();
+    // refresh();
     /* 종료 대기 */
     // mvprintw(term_height - 2, 2, "Press any key to exit...");
     getch();
