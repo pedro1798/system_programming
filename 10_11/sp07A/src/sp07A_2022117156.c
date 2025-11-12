f5f3e589eeb5f0d50e7bf492fc6a3c80b175c340
pedro1798 <peter584aa@knu.ac.kr>
2025-11-12 13:42:07 +0000

    sp07A complete

--- 10_11/sp07A/src/sp07A_2022117156.c
+++ 10_11/sp07A/src/sp07A_2022117156.c
@@ -6,61 +6,66 @@
 #include <sys/wait.h>
 
 int main() {
-    fprintf(stdout, "I am parents\n");
+    /* waitpid state 변수, pid 변수 선언 */
+    int son_st, daughter_st, grandson_st, granddaughter_st;
+    pid_t son, daughter, grandson, granddaughter;
     
-    int son_stat, daughter_stat, grandson_stat, granddaughter_stat;
-    pid_t pid_son, pid_daughter, pid_grandson, pid_granddaughter; 
+    fprintf(stdout, "I am parent.\n");
     
-    pid_son = fork(); // son fork
-    
-    if (pid_son == -1) { /* 실패 시 */
-        perror("fork");
-    } else if (pid_son == 0) { /* son 프로세스 스코프 */
+    son = fork(); // son fork();
+    if (son == -1) perror("fork"); // fork 에러처리
+    else if (son == 0) { // son 스코프
+        sleep(1); // 출력 순서 조정 
         fprintf(stdout, "I am son.\n");
-        
-        pid_grandson = fork(); // grandson fork
 
-        if (pid_grandson == -1) {
-            perror("perror");
-        } else if (pid_grandson == 0) { // grandson 프소세스 스코프
+        grandson = fork(); // grandson 출력 
+        if (grandson == -1) perror("fork"); // 에러처리 
+        else if (grandson == 0) { // grandson 스코프 
+            sleep(2);
             fprintf(stdout, "I am grandson.\n");
+            sleep(3);
             fprintf(stdout, "grandson exits.\n");
-            exit(1);
-        } else { // son 프로세스 스코프 
-            if (waitpid(pid_grandson, &grandson_stat, 0) == -1) { // grandson 종료될 때 까지 기다린다.
-                perror("waitpid");
+            exit(3); // grandson 종료 
+        } else { // son 스코프 
+            granddaughter = fork(); // granddaughter fork 
+            if (granddaughter == -1) perror("fork"); // 에러처리 
+            else if (granddaughter == 0) { // granddaughter 스코프
+                sleep(2);
+                fprintf(stdout, "I am granddaughter.\n");
+                sleep(3);
+                fprintf(stdout, "granddaughter eixts.\n");
+                exit(4); // granddaughter 종료 
+            } else { // son 스코프, 자식 종료 확인 
+                if (grandson == waitpid(grandson, &grandson_st, 0)) {
+                    fprintf(stdout, "son finished waiting for grandson.\n");
+                } // grandson 종료 확인 
+                if (granddaughter == waitpid(granddaughter, &granddaughter_st, 0)) {
+                    fprintf(stdout, "son finished waiting for granddaughter.\n");
+                } // granddaughter 종료 확인 
+                fprintf(stdout, "son eixts.\n");
+                exit(1); // son 종료 
             }
-        }
-        
-        pid_granddaughter = fork(); // granddaughter fork()
-        
-        if (pid_granddaughter == -1) perror("fork");
-        else if (pid_granddaughter == 0) { // granddaughter 프로세스 스코프
-            fprintf(stdout, "I am granddaughter.\n");
-            fprintf(stdout, "granddaughter exits.\n");
-            exit(1);
-        } else { // son 프로세스 스코프
-            /* granddaughter 종료될 때 까지 기다린다. */
-            if (waitpid(pid_granddaughter, &granddaughter_stat, 0) == -1) perror("waitpid");
-        }
-        
-        fprintf(stdout, "son exits.\n");
-        exit(1);
-    } else { // parent 프로세스
-        /* son 끝날 때 까지 기다린다. */
-        if (waitpid(pid_son, &son_stat, 0) == -1) perror("waitpid"); // 에러처리
-        
-        pid_daughter = fork(); // daughter fork()
-
-        if (pid_daughter == -1) perror("fork"); 
-        else if (pid_daughter == 0) { // daughter 프로세스 스코프 
+       }
+    } else { // parent 스코프 
+        daughter = fork(); // daughter fork 
+        if (daughter == -1) perror("fork"); // error handling 
+        else if (daughter == 0) { // daughter scope
+            sleep(1);
             fprintf(stdout, "I am daughter.\n");
+            sleep(6);
             fprintf(stdout, "daughter exits.\n");
-            exit(1);
-        } else { // parent 프로세스 스코프
-            if (waitpid(pid_daughter, &daughter_stat, 0) == -1) perror("waitpid"); 
+            exit(3); // daughter exits
+        } else { // parent scope 
+            if (son == waitpid(son, &son_st, 0)) { // 종료하면 그 프로세스의 pid 반환
+                fprintf(stdout, "parent finished waiting for son.\n");
+            } // son 종료 확인 
+            if (daughter == waitpid(daughter, &daughter_st, 0)) {
+                fprintf(stdout, "parent finished waiting for daughter.\n");
+            } // daughter 종료 확인 
+            /* if son and daughter exits */
         }
     }
-
+    /* 모든 자식 종료 확인 후 종료 */
     fprintf(stdout, "parent exits.\n");
+    return 0;
 }
