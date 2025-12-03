094939e993aec3e96ca7770f5d2935a312969c59
pedro1798 <peter584aa@knu.ac.kr>
2025-12-03 06:49:22 +0000

    update final template

--- sp_final_term_prepare/template/template_serv.c
+++ sp_final_term_prepare/template/template_serv.c
@@ -7,37 +7,51 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
+#define BACKLOG 1
 #define oops(msg) { perror(msg); exit(msg); }
 #define GRID_HEIGHT 10
 #define GRID_WIDTH 10
 
+typedef struct GameSize {
+   int box_height, box_width;
+   int start_x, start_y;
+   int term_height, term_width;
+} game_size_t;
+
 void init_ncurses();
 char** generate_grid(int box_height, int box_width);
 int socket_init(char* ip, int port); 
+game_size_t game_size_init();
 
 int main(int ac, char* argv[]) {
     if (ac != 3) {
-        fprintf(stderr, "parameter: <IP> <PORT>");
+        fprintf(stderr, "parameter: <IP> <PORT>\n");
         exit(1);
     }
 
+    int clnt_sock;
+    struct sockaddr_in clnt_addr;
+    socklen_t clnt_addr_size;
+    char buf[BUFSIZ];
+
     char **grid = generate_grid(box_height, box_width); 
+    /* bind까지 완료된 소켓 fd 리턴 */
     int serv_socket = socket_init(argv[1], argv[2]);
+    
+    if (listen(serv_socket, BACKLOG) == -1) oops("listen() error\n");  
+    
+    clnt_addr_size = sizeof(clnt_addr);
+    clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_addr, &clnt_addr_size);
+    if (clnt_sock == -1) oops("accept() error\n");
+    
+    game_size_t gzise = game_size_init();
     
     // ncurses start 
     init_ncurses();
-    int term_height, term_width;
-    getmaxyx(stdscr, term_height, term_width); // 터미널 크기 저장
 
-    int box_height = GRID_HEIGHT;
-    int box_width = GRID_WIDTH;
-
-    int start_y = (term_height - box_height) / 2; 
-    int start_x = (term_width - box_width) / 2; 
-
-    WINDOW *win = newwin(box_height, box_width, start_y, start_x);
-    box(win);
-
+    WINDOW *win = newwin(gsize.box_height, gsize.box_width, 
+                         gsize.start_y,    gsize.start_x);
+    box(win); 
 
 }
 
@@ -83,4 +97,18 @@ int socket_init(char* ip, int port) {
         oops("bind() error");
     }
     return sock;
+}
+
+game_size_t game_size_init() {
+    game_size_t gsize;
+    getmaxyx(stdscr, gsize.term_height, 
+            gsize.term_width); // 터미널 크기 저장
+    
+    gsize.box_height = GRID_HEIGHT;
+    gsize.box_width = GRID_WIDTH;
+
+    gsize.start_y = (term_height - box_height) / 2; 
+    gsize.start_x = (term_width - box_width) / 2; 
+    
+    return gsize;
 }
